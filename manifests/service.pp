1162e029149d4a9b9a34fabde752871719197cf4
Mark Trapp <mark.trapp@acquia.com>
2017-04-27 18:19:09 -0700

    Fix whitespace

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,9 @@
 # private
 class collectd::service {
-
   if $collectd::manage_service {
     service { 'collectd':
         ensure  => running,
         require => Package['collectd']
     }
   }
-
 }
